8222c4a8e1ea2c9566a32af62a677bbcedd1b616
Nathan-Ghedoni <105431420+Nathan-Ghedoni@users.noreply.github.com>
2022-05-16 09:24:54 -0400

    Update LOC.py

--- LOC.py
+++ LOC.py
@@ -2,7 +2,7 @@
 #Imported Libraries
 import math #allows this program to use arccos and cosine operations. As well as converting answers from radian to degrees
 #Functions
-def SSS():
+def SSS(): #This is the equation used if you know the length of all three sides of the triangle. The answer will show you the answer of A, which is the angle directly accross from the side.
     Sa = input("Enter First Side Value(a):") #Enter all values for sides
     Sb = input("Enter Second Side Value(b):")
     Sc = input("Enter Third Side Value(c):")
@@ -21,15 +21,17 @@ def SSS():
     else:
         print("Domain Error")
         Format_Menu()
-def SAS():
+def SAS(): #This is the equation used when you know two sides and the angle in between those sides. The answer will show the missing side. 
     SSb = input("Enter First Side Value(b):") #Enters side values of triangle
     SSc = input("Enter Second Side Value(c):")
     SSA = input("Enter the angle(degrees) in between the sides(CosA):") #Enter the angle between the two sides
     SS_b = float(SSb)#Converts input into decimals
     SS_c = float(SSc)
     SS_A = float(SSA)
-    SSS_A = math.degrees(SS_A)
-    a = ((SS_b**2 + SS_c**2) - (2*SS_b*SS_c*math.cos(SSS_A)))**(1/2) #Performs Law of Cosine equation based off input
+    Radian_Conversion = (math.pi/180) #Finding the cosine of any degree ends up getting converted to radians in python.
+    #To counteract this, the degree input must be converted to radians before finding the cosine of it.
+    #This will show the answer(side) in degree mode.
+    a = ((SS_b**2 + SS_c**2) - (2*SS_b*SS_c*math.cos(SS_A)))**(1/2) #Performs Law of Cosine equation based off input
     print(a)
 def Format_Menu():
     Format_Input = input("What data do you currently have?\n1.SSS\n2.SAS\nq.Quit Program\nEnter either 1,2, or q(Quit Program):")
