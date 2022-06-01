c890e7f6aea37d5ab845f1413fd2f5a2ea0e1b31
Nathan-Ghedoni <105431420+Nathan-Ghedoni@users.noreply.github.com>
2022-06-01 09:08:08 -0400

    Update LOC.py

--- LOC.py
+++ LOC.py
@@ -9,10 +9,10 @@ def SSS():
     Sa = input("Enter First Side Value(a):") #Enter all values for sides
     Sb = input("Enter Second Side Value(b):")
     Sc = input("Enter Third Side Value(c):")
-    Sa_Test = re.match("[0-9.]", Sa) #Checks if the input includes any set of numbers and or periods
-    Sb_Test = re.match("[0-9.]", Sb)
-    Sc_Test = re.match("[0-9.]", Sc)
-    if Sa_Test and Sb_Test and Sc_Test: #If each input is an integer or a decimal, continue the program, else new inputs will be requested
+    Sa_Test = re.match("^[0-9\.]*$", Sa) #Checks if the input includes any set of numbers and or periods
+    Sb_Test = re.match("^[0-9\.]*$", Sb)
+    Sc_Test = re.match("^[0-9\.]*$", Sc)
+    if Sa_Test and Sb_Test and Sc_Test and len(Sa) != 0 and len(Sb) != 0 and len(Sc) != 0: #If each input is an integer or a decimal, continue the program, else new inputs will be requested
         S_a = float(Sa) #Convert values into decimals
         S_b = float(Sb)
         S_c = float(Sc)
@@ -41,10 +41,10 @@ def SAS():
     SSb = input("Enter First Side Value(b):") #Enters side values of triangle
     SSc = input("Enter Second Side Value(c):")
     SSA = input("Enter the angle(degrees) in between the sides(CosA):") #Enter the angle between the two sides
-    SSb_Test = re.match("[0-9.]", SSb) ##Checks if the input includes any set of numbers and or periods
-    SSc_Test = re.match("[0-9.]", SSc)
-    SSA_Test = re.match("[0-9.]", SSA)
-    if SSb_Test and SSc_Test and SSA_Test: #If each input is an integer or a decimal, continue the program, else new inputs will be requested
+    SSb_Test = re.match("^[0-9\.]*$", SSb) ##Checks if the input includes any set of numbers and or periods
+    SSc_Test = re.match("^[0-9\.]*$", SSc)
+    SSA_Test = re.match("^[0-9\.]*$", SSA)
+    if SSb_Test and SSc_Test and SSA_Test and len(SSA) != 0 and len(SSb) != 0 and len(SSc) != 0: #If each input is an integer or a decimal, continue the program, else new inputs will be requested
         SS_b = float(SSb)#Converts input into decimals
         SS_c = float(SSc)
         SS_A = float(SSA)
